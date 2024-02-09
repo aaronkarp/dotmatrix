5a7ae988a3fa97eb1497a08971afbc6d9eb8089e
Aaron Karp <aaronkarp@outlook.com>
2024-02-09 14:11:05 -0500

    complete basic rendering

--- script.js
+++ script.js
@@ -1,73 +1,74 @@
 // Characters as map of binary strings
 const characterMap = {
-  "A": ["occbccc"],
-  "B": ["qccqccq"],
-  "C": ["ocfffco"],
-  "D": ["qcccccq"],
-  "E": ["bffqffb"],
-  "F": ["bffqfff"],
-  "G": ["ocf2cco"],
-  "H": ["cccbccc"],
-  "I": ["oeeeeeo"],
-  "J": ["iiiicco"],
-  "K": ["cyxjxyc"],
-  "L": ["ffffffb"],
-  "M": ["csttccc"],
-  "N": ["czt1ccc"],
-  "O": ["occccco"],
-  "P": ["qccqfff"],
-  "Q": ["occcty5"],
-  "R": ["qccqxyc"],
-  "S": ["ocfoico"],
-  "T": ["beeeeee"],
-  "U": ["cccccco"],
-  "V": ["cccccde"],
-  "W": ["cccttsc"],
-  "X": ["ccdedcc"],
-  "Y": ["cccdeee"],
-  "Z": ["bihegfb"],
-  ".": ["aaaaajj"],
-  ",": ["aaaaagf"],
-  "?": ["ociheae"],
-  "!": ["eeeeeae"],
-  "@": ["octst1o"],
-  "#": ["adbdbda"],
-  "$": ["erxowqe"],
-  "%": ["jzheg1m"],
-  "^": ["aaedcaa"],
-  "&": ["kcdeuy5"],
-  "+": ["aeeoeea"],
-  "=": ["aababaa"],
-  "*": ["atoboca"],
-  "/": ["aihegfa"],
-  "\\": ["afgehia"],
-  "|": ["eeeeeee"],
-  "\"": ["dddaaaa"],
-  "'": ["eeeaaaa"],
-  ":": ["aaeeaeg"],
-  ";": ["aaeeaee"],
-  "<": ["ihegehi"],
-  ">": ["fgehegf"],
-  "[": ["bfffffb"],
-  "]": ["biiiiib"],
-  "{": ["pggfggp"],
-  "}": ["nhhihhn"],
-  "(": ["pgggggp"],
-  ")": ["nhhhhhn"],
-  "1": ["ekeeeeo"],
-  "2": ["ocihegb"],
-  "3": ["ocioico"],
-  "4": ["cccbiii"],
-  "5": ["bffqiiq"],
-  "6": ["offqcco"],
-  "7": ["biheeee"],
-  "8": ["occocco"],
-  "9": ["occrico"],
-  "0": ["oc1tzco"]
+  "A": "occbccc",
+  "B": "qccqccq",
+  "C": "ocfffco",
+  "D": "qcccccq",
+  "E": "bffqffb",
+  "F": "bffqfff",
+  "G": "ocf2cco",
+  "H": "cccbccc",
+  "I": "oeeeeeo",
+  "J": "iiiicco",
+  "K": "cyxjxyc",
+  "L": "ffffffb",
+  "M": "csttccc",
+  "N": "czt1ccc",
+  "O": "occccco",
+  "P": "qccqfff",
+  "Q": "occcty5",
+  "R": "qccqxyc",
+  "S": "ocfoico",
+  "T": "beeeeee",
+  "U": "cccccco",
+  "V": "cccccde",
+  "W": "cccttsc",
+  "X": "ccdedcc",
+  "Y": "cccdeee",
+  "Z": "bihegfb",
+  ".": "aaaaajj",
+  ",": "aaaaagf",
+  "?": "ociheae",
+  "!": "eeeeeae",
+  "@": "octst1o",
+  "#": "adbdbda",
+  "$": "erxowqe",
+  "%": "jzheg1m",
+  "^": "aaedcaa",
+  "&": "kcdeuy5",
+  "+": "aeeoeea",
+  "=": "aababaa",
+  "*": "atoboca",
+  "/": "aihegfa",
+  "\\": "afgehia",
+  "|": "eeeeeee",
+  "\"": "dddaaaa",
+  "'": "eeeaaaa",
+  ":": "aaeeaeg",
+  ";": "aaeeaee",
+  "<": "ihegehi",
+  ">": "fgehegf",
+  "[": "bfffffb",
+  "]": "biiiiib",
+  "{": "pggfggp",
+  "}": "nhhihhn",
+  "(": "pgggggp",
+  ")": "nhhhhhn",
+  "1": "ekeeeeo",
+  "2": "ocihegb",
+  "3": "ocioico",
+  "4": "cccbiii",
+  "5": "bffqiiq",
+  "6": "offqcco",
+  "7": "biheeee",
+  "8": "occocco",
+  "9": "occrico",
+  "0": "oc1tzco",
+  " ": "aaaaaaa"
 }
 
 // Binary strings
-const gridMap = {
+const rowMap = {
   "a": "00000",
   "b": "11111",
   "c": "10001",
@@ -102,69 +103,26 @@ const gridMap = {
   "6": "01001"
 }
 
-const dotmatrix = (container, cols = 20, rows = 3) => {
-  // Select the element where the sign will be mounted
-  const containerEl = document.querySelector(container);
-
-  // Create the main container for the sign and give it the dotMatrixSign class
-  const signStructure = document.createElement('div');
-  signStructure.classList.add('dotMatrixSign', 'hidden');
-
-  // Generate the cells of the sign and add them to the main sign structure container
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      const cell = document.createElement('div');
-
-      // Generate the actual dot matrix in each cell
-      for (let r = 0; r < 9; r++) {
-        for (let c = 0; c < 7; c++) {
-          const dot = document.createElement('div');
-
-          const dotId = `c${j + 1}r${r + 1}d${c + 1}`;
-          dot.setAttribute('id', dotId);
-          if (r === 0 || r === 8 || c === 0 || c === 6) {
-            dot.classList.add('dot--dark');
-          } else {
-            dot.classList.add('dot');
-          }
-
-          cell.appendChild(dot);
-        }
-      }
-
-      const cellId = `c${j + cols * i}`;
-      cell.classList.add('dotMatrix__cell');
-      cell.setAttribute('id', cellId);
-
-      // Calculate cell width
-      cell.style.width = `${100 / cols}%`;
-
-      // Add cell to sign structure
-      signStructure.appendChild(cell);
-    }
-  }
-
-  // Add the sign to the DOM
-  containerEl.appendChild(signStructure);
-  const dotDimension = signStructure.offsetWidth * (1 / cols) * (1 / 7);
-  const allDots = document.querySelectorAll('.dot, .dot--dark');
-  allDots.forEach(dot => {
-    dot.style.width = dotDimension + 'px';
-    dot.style.height = dotDimension + 'px';
-  });
-  signStructure.classList.remove('hidden');
+const dotmatrix = (container, cols = 20, rows = 3, message) => {
 
   const padString = (padStr) => {
     let strLength = cols - padStr.length;
     if (strLength > 1) {
-      for (let i = 1; i < (strLength / 2); i++) {
+      for (let i = 1; i <= (strLength / 2); i++) {
         padStr = ` ${padStr} `;
+      }
+    }
+    strLength = cols - padStr.length;
+    if (strLength > 0) {
+      for (let i = 0; i < strLength; i++) {
+        padStr = `${padStr} `;
       }
     }
     return padStr;
   }
 
   const buildStringArray = (str) => {
+    str = str.split(' ');
     let subStr = '';
     let strArray = [];
     let tempStr = '';
@@ -185,10 +143,54 @@ const dotmatrix = (container, cols = 20, rows = 3) => {
         subStr = word;
       }
     }
-    console.log(strArray);
+    return strArray;
   }
 
-  const message = "I AM THE VERY MODEL OF A MODERN MAJOR GENERAL. I'VE INFORMATION VEGETABLE, ANIMAL, AND MINERAL.";
-  
-  buildStringArray(message.split(' '));
+  const renderChar = (char, cell) => {
+    const charPlan = `a${characterMap[char]}a`;
+    for (let r = 0; r < charPlan.length; r++) {
+      const rowPlan = `0${rowMap[charPlan[r]]}0`;
+      for (let d = 0; d < rowPlan.length; d++) {
+        const dot = document.createElement('div');
+        dot.classList.add(rowPlan[d] === '0' ? 'dot--dark' : 'dot');
+        cell.appendChild(dot);
+      }
+    }
+  }
+
+  // Select the element where the sign will be mounted
+  const containerEl = document.querySelector(container);
+
+  // Format the message
+  const messageArr = buildStringArray(message);
+
+  // Create the main container for the sign and give it the dotMatrixSign class
+  const signStructure = document.createElement('div');
+  signStructure.classList.add('dotMatrixSign', 'hidden');
+
+  // Generate the cells of the sign and add them to the main sign structure container
+  for (let i = 0; i < messageArr.length; i++) {
+    for (let j = 0; j < messageArr[i].length; j++) {
+      const cell = document.createElement('div');
+      // Generate the actual dot matrix in each cell
+      renderChar(messageArr[i][j], cell);
+      cell.classList.add('dotMatrix__cell');
+
+      // Calculate cell width
+      cell.style.width = `${100 / cols}%`;
+
+      // Add cell to sign structure
+      signStructure.appendChild(cell);
+    }
+  }
+
+  // Add the sign to the DOM
+  containerEl.appendChild(signStructure);
+  const dotDimension = signStructure.offsetWidth * (1 / cols) * (1 / 7);
+  const allDots = document.querySelectorAll('.dot, .dot--dark');
+  allDots.forEach(dot => {
+    dot.style.width = dotDimension + 'px';
+    dot.style.height = dotDimension + 'px';
+  });
+  signStructure.classList.remove('hidden');
 }
